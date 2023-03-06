63816fa55135258b89f246d950900ffb729d5204
gavand-sagar <gavand.sagar.s@gmail.com>
2023-03-06 16:53:00 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Shortcuts
+{
+    public class Product
+    {
+        public string Brand { get; set; }
+        public string ProductName { get; set; }
+        public double Price { get; set; }
+
+        public void InitializeFromConsole()
+        {
+            ProductName = SagarUtilities.GetStringFromUser("Enter Product Name");
+            Brand = SagarUtilities.GetStringFromUser("Enter Brand Name");
+            Price = SagarUtilities.GetDoubleFromUser("Enter Price");
+        }
+    }
+
+    public class Mobile
+    {
+        public string MobileBrand { get; set; }
+        public string MobileName { get; set; }
+        public double MobilePrice { get; set; }
+
+
+    }
+
+    public class Program
+    {
+
+
+
+
+        static void Main(string[] args)
+        {
+            List<Product> products = GetProductsFromUser();
+
+            List<Mobile> mobiles = ConvertProductListToMobile(products);
+
+            DisplayMobileList(mobiles);
+
+            Console.ReadLine();
+
+        }
+
+        private static void DisplayMobileList(List<Mobile> mobiles)
+        {
+            Console.Clear();
+            Console.WriteLine("{0,-15} {1,-15} {2,-15}", "Name", "Brand", "Price");
+            Console.WriteLine("==================================");
+            foreach (var item in mobiles)
+            {
+                Console.WriteLine("{0,-15} {1,-15} {2,-15}", item.MobileName, item.MobileBrand, item.MobilePrice);
+            }
+        }
+
+        private static List<Mobile> ConvertProductListToMobile(List<Product> products)
+        {
+            List<Mobile> mobiles = new List<Mobile>();
+
+            foreach (var product in products)
+            {
+                Mobile mobile = new Mobile();
+                mobile.MobileBrand = product.Brand;
+                mobile.MobileName = product.ProductName;
+                mobile.MobilePrice = product.Price;
+                mobiles.Add(mobile);
+            }
+
+            return mobiles;
+        }
+
+        private static List<Product> GetProductsFromUser()
+        {
+            List<Product> products = new List<Product>();
+            do
+            {
+                Product product = new Product();
+
+                product.InitializeFromConsole();
+
+                products.Add(product);
+
+                Console.WriteLine("Do you want to add new? (enter to continue | 'n' to stop)");
+
+            } while (Console.ReadLine().ToLower() != "n");
+
+            return products;
+        }
+
+
+    }
+}
---
+++ SagarUtilities.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shortcuts
+{
+    public static class SagarUtilities
+    {
+        public static double GetDoubleFromUser(string v)
+        {
+            Console.WriteLine(v);
+            try
+            {
+                return double.Parse(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid Entry");
+                return GetDoubleFromUser(v);
+            }
+        }
+
+        public static string GetStringFromUser(string message)
+        {
+            Console.WriteLine(message);
+            string input = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(input))
+            {
+                Console.WriteLine("Invalid Entry");
+                return GetStringFromUser(message);
+            }
+            else
+            {
+                return input;
+            }
+        }
+    }
+}
